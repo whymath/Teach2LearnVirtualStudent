c1a97fb85cbc42dfe7224b8862afd4d44e74a42e
whymath <20425672+whymath@users.noreply.github.com>
2024-05-15 14:20:25 -0400

    Reverting to initial code to resolve Assistants API file_ids error

--- app.py
+++ app.py
@@ -3,67 +3,18 @@ import chainlit as cl
 from dotenv import load_dotenv
 import utils
 
-from openai import AsyncOpenAI
-import time
-from langchain.agents.openai_assistant import OpenAIAssistantRunnable
-
 
 load_dotenv()
 
 
-start_msg = "Teach2Learn Virtual Student by Jerry Chiang and Yohan Mathew\n\nYou can choose to upload a PDF, or just start chatting"
-# instructions = "You are a helpful assistant"
-instructions = "You are a virtual student being taught by the user. You can ask clarifying questions to better understand the user's explanation. Your goal is to ensure that the user understands the concept they are explaining. You can also ask questions to help the user elaborate on their explanation. You can ask questions like 'Can you explain that in simpler terms?' or 'Can you provide an example?'."
-# client = AsyncOpenAI()
-# assistant = client.beta.assistants.create(
-#     name="T2L Virtual Student",
-#     instructions=instructions,
-#     model="gpt-3.5-turbo",
-# )
-assistant = OpenAIAssistantRunnable.create_assistant(
-    name="T2L Virtual Student",
-    instructions=instructions,
-    model="gpt-3.5-turbo",
-    tools=[{"type": "code_interpreter"}],
-    tool_resources={
-        "code_interpreter": {
-            "file_ids": ["file-OKohewH4QI0GidqqJzxrkwpj"]
-        }
-    }
-)
-print("assistant =", assistant)
-# thread = client.beta.threads.create()
-# print("thread =", thread)
-
-
 @cl.on_chat_start
 async def start_chat():
-
-    # Create an OpenAI assistant
-    # instructions = "You are a helpful assistant"
-    # client = AsyncOpenAI()
-    # assistant = client.beta.assistants.create(
-    #     name="T2L Virtual Student",
-    #     instructions=instructions,
-    #     model="gpt-3.5-turbo",
-    # )
-    # thread = client.beta.threads.create()
-
-    # Store the assistant and thread in the user session
-    # settings = {
-    #     "instructions": instructions,
-    #     "client": client,
-    #     "assistant": assistant,
-    #     "thread": thread
-    # }
-    settings = {}
+    # Create the RAQA chain and store it in the user session
+    raqa_chain = utils.create_raqa_chain_from_docs()
+    settings = {
+        "chain": raqa_chain
+    }
     cl.user_session.set("settings", settings)
-
-    # Send a welcome message with an action button
-    # actions = [
-    #     cl.Action(name="upload_pdf", value="upload_pdf_value", label="Upload a PDF", description="Upload a PDF")
-    # ]
-    # await cl.Message(content=start_msg, actions=actions).send()
 
 
 @cl.on_message
@@ -74,89 +25,13 @@ async def main(message: cl.Message):
 
     # Get the chain from the user session
     settings = cl.user_session.get("settings")
-    # instructions = settings["instructions"]
-    # client = settings["client"]
-    # assistant = settings["assistant"]
-    # thread = settings["thread"]
-    try:
-        raqa_chain = settings["raqa_chain"]
-    except KeyError:
-        raqa_chain = None
+    raqa_chain = settings["chain"]
 
     # Generate the response from the chain
-    if raqa_chain:
-        print("Using RAQA chain to generate response")
-        query_response = raqa_chain.invoke({"question" : user_query})
-        query_answer = query_response["response"].content
-    else:
-        print("Using OpenAI assistant to generate response")
-        # message = client.beta.threads.messages.create(
-        #     thread_id=thread.id,
-        #     role="user",
-        #     content=user_query
-        # )
-        # print("message =", message)
-        # run = client.beta.threads.runs.create(
-        #     thread_id=thread.id,
-        #     assistant_id=assistant.id,
-        #     instructions=instructions
-        # )
-        # print("run =", run)
-        # while run.status == "in_progress" or run.status == "queued":
-        #     time.sleep(1)
-        #     run = client.beta.threads.runs.retrieve(
-        #         thread_id=thread.id,
-        #         run_id=run.id
-        #     )
-        # print("run.status =", run.status)
-        # messages = client.beta.threads.messages.list(
-        #     thread_id=thread.id
-        # )
-        # # print("messages =", messages)
-        # print("messages.data =", messages.data)
-        # query_answer = messages.data[0].content
-
-        query_response = assistant.invoke({"content": user_query})
-        query_answer = query_response["response"].content
-
-        # run = client.beta.threads.runs.create(
-        #     thread_id=thread.id,
-        #     assistant_id=assistant.id
-        # )
+    query_response = raqa_chain.invoke({"question" : user_query})
+    query_answer = query_response["response"].content
+    print('query_answer =', query_answer)
     
     # Create and send the message stream
-    print('query_answer =', query_answer)
     msg = cl.Message(content=query_answer)
     await msg.send()
-
-
-@cl.action_callback("upload_pdf")
-async def upload_pdf_fn(action: cl.Action):
-    print("The user clicked on the action button!")
-
-    files = None
-
-    # Wait for the user to upload a file
-    while files == None:
-        files = await cl.AskFileMessage(
-            content="Waiting for file selection",
-            accept=["application/pdf"],
-            max_size_mb=20,
-            timeout=180,
-        ).send()
-
-    file = files[0]
-
-    msg = cl.Message(
-        content=f"Processing `{file.name}`...", disable_human_feedback=True
-    )
-    await msg.send()
-
-    # Create the RAQA chain and store it in the user session
-    raqa_chain = utils.create_raqa_chain_from_docs(file)
-    settings = {
-        "raqa_chain": raqa_chain
-    }
-    cl.user_session.set("settings", settings)
-
-    return "Thank you for clicking on the action button!"
--- utils.py
+++ utils.py
@@ -28,35 +28,38 @@ def chunk_documents(docs, tiktoken_len):
     return split_chunks
 
 
-def create_raqa_chain_from_docs(docs):
+def create_raqa_chain_from_docs():
+    # Load the documents from a PDF file using PyMuPDFLoader
+    # docs = PyMuPDFLoader("data/c7318154-f6ae-4866-89fa-f0c589f2ee3d.pdf").load()
+    docs = PyMuPDFLoader("https://d18rn0p25nwr6d.cloudfront.net/CIK-0001326801/c7318154-f6ae-4866-89fa-f0c589f2ee3d.pdf").load()
+    print("Loaded", len(docs), "documents")
+    print(docs[0])
 
-    # # Load the documents from a PDF file using PyMuPDFLoader
-    # docs = PyMuPDFLoader("https://d18rn0p25nwr6d.cloudfront.net/CIK-0001326801/c7318154-f6ae-4866-89fa-f0c589f2ee3d.pdf").load() # TODO: Update this to enable user to upload PDF
-    # print("Loaded", len(docs), "documents")
-    # print(docs[0])
-
-    # Create a Qdrant vector store from the split chunks and embedding model, and obtain its retriever
+    # Chunk documents, load embedding model, create vectorstore and retriever
     split_chunks = chunk_documents(docs, tiktoken_len)
     embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
     qdrant_vectorstore = Qdrant.from_documents(
         split_chunks,
         embedding_model,
         location=":memory:",
-        collection_name="LoadedPDF",
+        collection_name="Meta 10-k Filings",
     )
     qdrant_retriever = qdrant_vectorstore.as_retriever()
 
     # Define the RAG prompt template
+    openai_chat_model = ChatOpenAI(model="gpt-3.5-turbo")
     RAG_PROMPT = """
-    Assume you are a virtual student being taught by the user. You can ask clarifying questions to better understand the user's explanation. Your goal is to ensure that the user understands the concept they are explaining. You can also ask questions to help the user elaborate on their explanation. You can ask questions like "Can you explain that in simpler terms?" or "Can you provide an example?".
+    CONTEXT:
+    {context}
 
-    USER MESSAGE:
+    QUERY:
     {question}
+
+    Use the provided context to answer the provided user query. Only use the provided context to answer the query. If you do not know the answer, respond with "I don't know".
     """
     rag_prompt = ChatPromptTemplate.from_template(RAG_PROMPT)
 
-    # Create the retrieval augmented QA chain using the Qdrant retriever, RAG prompt, and OpenAI chat model
-    openai_chat_model = ChatOpenAI(model="gpt-3.5-turbo")
+    # Define the RAQA chain
     retrieval_augmented_qa_chain = (
         {"context": itemgetter("question") | qdrant_retriever, "question": itemgetter("question")}
         | RunnablePassthrough.assign(context=itemgetter("context"))
