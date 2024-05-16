5768c9adf61f6749036935fb74c1f2d4db065948
whymath <20425672+whymath@users.noreply.github.com>
2024-05-16 17:58:29 -0400

    Adding few shot examples and refining prompts

--- app.py
+++ app.py
@@ -10,12 +10,18 @@ load_dotenv()
 
 start_msg = "Hello! I'm Teach2Learn VirtualStudent, a virtual student peer by Jerry Chiang and Yohan Mathew\n\nYou can choose to upload a PDF, or just start chatting\n"
 base_instructions = """
-Assume you are a student and that the user is your teacher. Your goal is to ensure that the user understands the concept they are explaining.
-You should always first let the user know if they are correct or not, and then ask them questions to help them learn by teaching rather than explaining things to them.
+Assume you have mastery in the topic and that the user is a someone who is trying to ensure they have a solid understanding by teaching and explaining the material to you.
+Your goal is to ensure that the user understands the concept they are explaining by asking questions to help them learn by teaching rather than explaining things directly to them.
+Let the user know if they are correct.  If the user is wrong or off track, you should challenge the user by asking them Socratic questions to guide them back.
 If they ask for feedback, you should provide constructive feedback on the whole conversation instead of asking another question.
 """
-aistudent_instructions = """
-Pretend you are a student and that the user is your teacher. Your goal is to get the user to teach you about a topic or concept, and you can ask clarifying questions to help them teach better.
+ai_student_instructions = """
+Pretend you are a bumbling student with a poor grasp of the topic, are prone to make mistakes, and the user is your teacher.
+Your goal is to get the user to teach you about a topic or concept, and you can ask clarifying questions to help them teach better.
+You may lay out a scneario for the teacher to help you thru, such as a homework problem, a scenario you need to resolve, or a piece of text you need help deciphering.
+Do not explain the material to them except when they ask you to, and when you do as a bumbling student, you may make mistakes and say something unclear or false.
+If they ask for feedback, instead of asking another question, you should provide constructive feedback on how well they grasped the content and did in their teaching, including ways they can improve.
+When you make a mistake, if the user does not catch or correct you, make sure you let the user know during the feedback at the end of the session.
 """
 openai_chat_model = ChatOpenAI(model="gpt-3.5-turbo")
 base_chain = utils.create_base_chain(openai_chat_model, base_instructions)
@@ -33,8 +39,8 @@ async def start_chat():
 
     # Send a welcome message with action buttons
     actions = [
-        cl.Action(name="switch_default", value="switch_default_value", label="Switch back to default mode (or for feedback)", description="Switch back to default mode (or for feedback)"),
-        cl.Action(name="switch_ai_student", value="switch_ai_student_value", label="Switch to AI student mode", description="Switch to AI student mode"),
+        cl.Action(name="switch_default", value="switch_default_value", label="Switch back to default mode", description="Switch back to default mode"),
+        cl.Action(name="switch_ai_student", value="switch_ai_student_value", label="Switch to bumbling student mode", description="Switch to bumbling student mode"),
         cl.Action(name="upload_pdf", value="upload_pdf_value", label="Upload a PDF", description="Upload a PDF")
     ]
     await cl.Message(content=start_msg, actions=actions).send()
@@ -88,7 +94,7 @@ async def upload_pdf_fn(action: cl.Action):
 
     # Create the RAG chain and store it in the user session
     if settings["current_mode"] == "ai_student_chain":
-        rag_instructions = aistudent_instructions
+        rag_instructions = ai_student_instructions
     else:
         rag_instructions = base_instructions
     rag_chain = utils.create_rag_chain_from_file(openai_chat_model, rag_instructions, file_uploaded, file_uploaded.name)
@@ -108,7 +114,7 @@ async def switch_default_fn(action: cl.Action):
     settings["rag_chain_available"] = False
     cl.user_session.set("settings", settings)
 
-    msg = cl.Message(content="Okay, I'm back to answering general questions. What would you like to try teaching me next?")
+    msg = cl.Message(content="Okay, I'm back to my default mode. What would you like to try teaching me next?")
     await msg.send()
 
 
@@ -117,10 +123,10 @@ async def switch_ai_student_fn(action: cl.Action):
     print("\nSwitching to AI student mode")
     settings = cl.user_session.get("settings")
 
-    ai_student_chain = utils.create_base_chain(openai_chat_model, aistudent_instructions)
+    ai_student_chain = utils.create_ai_student_chain(openai_chat_model, ai_student_instructions)
     settings["ai_student_chain"] = ai_student_chain
     settings["current_mode"] = "ai_student_chain"
     cl.user_session.set("settings", settings)
 
-    msg = cl.Message(content="Okay, I will take on the role of a student. What would you like to try teaching me next?")
+    msg = cl.Message(content="Okay, I will take on the role of an unsure student. What would you like to try teaching me next?")
     await msg.send()
--- utils.py
+++ utils.py
@@ -27,6 +27,7 @@ def chunk_documents(docs, tiktoken_len):
     print('len(split_chunks) =', len(split_chunks))
     return split_chunks
 
+
 def process_file(file: AskFileResponse):
     import tempfile
 
@@ -44,11 +45,64 @@ def create_base_chain(openai_chat_model, system_prompt):
     human_template = "{question}"
     base_prompt = ChatPromptTemplate.from_messages([
         ("system", system_prompt),
+        # Example 1
+        # ("human", "I want to teach you about the Pythagorean Theorem. Can you pretend to know the topic well and give me feedback on how well I explain it?"),
+        # ("ai", "That sounds great! I’m here to learn about the Pythagorean Theorem from you. Can you explain what the Pythagorean Theorem is and how to apply it?"),
+        # Example 2
+        # ("human", "The Pythagorean Theorem is a theorem that relates the lengths of right triangles. More specifically, if a triangle has 3 sides - a, b and c, with c being the hypotenuse - then the theorem tells us a^2+b^2 = c^2. This helps us calculate distances in 2-D space and has applications in math, science, engineering, and architecture."),
+        # ("ai", "Great! That makes sense. Can you walk me through an example of how to apply the Pythagorean Theorem to a real world problem?"),
+        # Example 3
+        # ("human", "The Pythagorean Theorem has something to do with triangles and the lengths of their sides. I'm not sure what though."),
+        # ("ai", "Okay, I see. What kind of triangles does it deal with? And what is the relationship between the three sides? Maybe this site can help us: https://byjus.com/maths/pythagoras-theorem"),
+        # Example 4
+        ("human", "I'd like to end the session"),
+        ("ai", "No worries.  Would you like me to share some feedback with you?"),
+        # Example 5
+        # ("human", "I don't want to discuss the Pythagorean Theorem anymore.  Instead, I want to talk more about circles."),
+        # ("ai", "That's fine.  Would you like for me to first give you some feedback on this lesson before we switch to another topic?"),
+        # Example 6
+        ("human", "Can you tell me how I did?"),
+        ("ai", "Sure! Shall I first give you some feedback on how well you covered the content, and then some feedback on your approach to teaching?"),
+        # Example 7
+        ("human", "Can you tell me the answer?"),
+        ("ai", "Hmm, maybe we can figure it out together? If I passed you some references to look up, can you help me figure it out?"),
+        # Example 8a (mistake)
+        ("human", "So using the Pythagorean Theorem, given the hypotenuse is 13 and one of the legs is 5, we know the length of the other leg is going to equal sqrt(13^2 - 6^2) = sqrt(169 - 36) = sqrt(133) which is almost 12?"),
+        ("ai", "Hmm, can you explain to me why you have written 6^2 rather than 5^2?"),
         ("human", human_template)
     ])
     base_chain = base_prompt | openai_chat_model
     print("Created base chain\n")
     return base_chain
+
+
+def create_ai_student_chain(openai_chat_model, system_prompt):
+    human_template = "{question}"
+    base_prompt = ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        # Example 3
+        # ("human", "The Pythagorean Theorem has something to do with triangles and the lengths of their sides. I'm not sure what though."),
+        # ("ai", "Okay, I see. What kind of triangles does it deal with? And what is the relationship between the three sides? Maybe this site can help us: https://byjus.com/maths/pythagoras-theorem"),
+        # Example 4
+        ("human", "I'd like to end the session"),
+        ("ai", "No worries.  Would you like me to share some feedback with you?"),
+        # Example 5
+        # ("human", "I don't want to discuss the Pythagorean Theorem anymore.  Instead, I want to talk more about circles."),
+        # ("ai", "That's fine.  Would you like for me to first give you some feedback on this lesson before we switch to another topic?"),
+        # Example 6
+        ("human", "Can you tell me how I did?"),
+        ("ai", "Sure! Shall I first give you some feedback on how well you covered the content, and then some feedback on your approach to teaching?"),
+        # Example 7
+        ("human", "Can you tell me the answer?"),
+        ("ai", "Hmm, maybe we can figure it out together? If I passed you some references to look up, can you help me figure it out?"),
+        # Example 8b (mistake)
+        ("human", "So can you show me how you would apply the Pythagorean Theorem to solve this next problem? Let's say you are building a 8 feet tall vertical structure and you'd like to add support beams all around it 6 feet away from its base. Can you help me calculate how long these support beams should be?"),
+        ("ai", "Because the structure is vertical and the support beams are on the ground, we see this forms a right triangle. So we can use the Pythagorean Theorem to calculate the length of the support beam. Let's call the length of the support beam 'c', while the height of the vertical structure is 'a' and the distance the support beam is away from the structure is 'b'.  Hence, if c^2 = a^2 + b^2, I think we need to solve for c = sqrt(8^2 + 6^2) = sqrt(16+12) = sqrt (28) = 5.3?  Did I do that right?"),
+        ("human", human_template)
+    ])
+    ai_student_chain = base_prompt | openai_chat_model
+    print("Created base chain\n")
+    return ai_student_chain
 
 
 def create_rag_chain_from_file(openai_chat_model, base_instructions, file_response, file_name):
@@ -74,7 +128,7 @@ def create_rag_chain_from_file(openai_chat_model, base_instructions, file_respon
     # Define the RAG prompt template
     RAG_PROMPT = """
     Use the provided context while replying to the user query. Only use the provided context to respond to the query.
-    If the context is not sufficient, you can respond with "I cannot seem to find this topic in the PDF. Would you like to switch to another mode?".
+    If the context is not sufficient, you can respond with "I cannot seem to find this topic in the PDF. Would you like to switch to back to the default or bumbling student mode?".
 
     QUERY:
     {question}
