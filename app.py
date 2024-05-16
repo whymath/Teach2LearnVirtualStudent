701043336a821b519bdb883573a2b23358aafa06
whymath <20425672+whymath@users.noreply.github.com>
2024-05-15 21:57:46 -0400

    Primary fixes for upload functionality

--- app.py
+++ app.py
@@ -7,31 +7,85 @@ import utils
 load_dotenv()
 
 
+start_msg = "Teach2Learn Virtual Student by Jerry Chiang and Yohan Mathew\n\nYou can choose to upload a PDF, or just start chatting"
+
+# Create the RAQA chain and store it in the user session
+raqa_chain = utils.create_raqa_chain_from_docs()
+
+
 @cl.on_chat_start
 async def start_chat():
-    # Create the RAQA chain and store it in the user session
-    raqa_chain = utils.create_raqa_chain_from_docs()
-    settings = {
-        "chain": raqa_chain
-    }
-    cl.user_session.set("settings", settings)
+    # # Create the RAQA chain and store it in the user session
+    # raqa_chain = utils.create_raqa_chain_from_docs()
+    # settings = {
+    #     "chain": raqa_chain
+    # }
+    # cl.user_session.set("settings", settings)
+    print("Chat started")
+
+    # Send a welcome message with an action button
+    actions = [
+        cl.Action(name="upload_pdf", value="upload_pdf_value", label="Upload a PDF", description="Upload a PDF")
+    ]
+    await cl.Message(content=start_msg, actions=actions).send()
 
 
 @cl.on_message
 async def main(message: cl.Message):
     # Print the message content
     user_query = message.content
-    print('user_query =', user_query)
+    print('\nuser_query =', user_query)
 
     # Get the chain from the user session
-    settings = cl.user_session.get("settings")
-    raqa_chain = settings["chain"]
+    try:
+        settings = cl.user_session.get("settings")
+        raqa_chain_upload = settings["raqa_chain_upload"]
+    except Exception as e:
+        print("Error fetching chain from session, defaulting to base chain", e)
+        raqa_chain_upload = None
 
     # Generate the response from the chain
-    query_response = raqa_chain.invoke({"question" : user_query})
+    if raqa_chain_upload:
+        print("\nUsing UPLOAD chain to answer query", user_query)
+        query_response = raqa_chain_upload.invoke({"question" : user_query})
+    else:
+        print("\nUsing DEFAULT chain to answer query", user_query)
+        query_response = raqa_chain.invoke({"question" : user_query})
     query_answer = query_response["response"].content
-    print('query_answer =', query_answer)
+    print('query_answer =', query_answer, '\n')
     
     # Create and send the message stream
     msg = cl.Message(content=query_answer)
+    await msg.send()
+
+
+@cl.action_callback("upload_pdf")
+async def upload_pdf_fn(action: cl.Action):
+    print("\nThe user clicked on an action button!")
+
+    files = None
+
+    # Wait for the user to upload a file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content="Processing your file",
+            accept=["application/pdf"],
+            max_size_mb=20,
+            timeout=180,
+        ).send()
+
+    file_uploaded = files[0]
+    print("\nUploaded file:", file_uploaded, "\n")
+
+    # Create the RAQA chain and store it in the user session
+    filepath_uploaded = file_uploaded.path
+    filename_uploaded = file_uploaded.name
+    raqa_chain_upload = utils.create_raqa_chain_from_file(filepath_uploaded, filename_uploaded)
+
+    settings = {
+        "raqa_chain_upload": raqa_chain_upload
+    }
+    cl.user_session.set("settings", settings)
+
+    msg = cl.Message(content="Thank you for uploading!")
     await msg.send()
--- utils.py
+++ utils.py
@@ -82,5 +82,55 @@ def create_raqa_chain_from_docs():
         | RunnablePassthrough.assign(context=itemgetter("context"))
         | {"response": rag_prompt | openai_chat_model, "context": itemgetter("context")}
     )
+    print("Created retrieval augmented QA chain from default PDF file")
+
+    return retrieval_augmented_qa_chain
+
+
+def create_raqa_chain_from_file(filepath_uploaded, filename_uploaded):
+
+    # # Load the documents from a PDF file using PyMuPDFLoader
+    # docs = PyMuPDFLoader("https://d18rn0p25nwr6d.cloudfront.net/CIK-0001326801/c7318154-f6ae-4866-89fa-f0c589f2ee3d.pdf").load()
+    docs = PyMuPDFLoader(filepath_uploaded).load()
+    print("Loaded", len(docs), "documents")
+    print(docs[0])
+
+    # Create a Qdrant vector store from the split chunks and embedding model, and obtain its retriever
+    split_chunks = chunk_documents(docs, tiktoken_len)
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    qdrant_vectorstore = Qdrant.from_documents(
+        split_chunks,
+        embedding_model,
+        location=":memory:",
+        collection_name="LoadedPDF",
+    )
+    qdrant_retriever = qdrant_vectorstore.as_retriever()
+
+    # Define the RAG prompt template
+    # RAG_PROMPT = """
+    # Assume you are a virtual student being taught by the user. You can ask clarifying questions to better understand the user's explanation. Your goal is to ensure that the user understands the concept they are explaining. You can also ask questions to help the user elaborate on their explanation. You can ask questions like "Can you explain that in simpler terms?" or "Can you provide an example?".
+
+    # USER MESSAGE:
+    # {question}
+    # """
+    RAG_PROMPT = """
+    CONTEXT:
+    {context}
+
+    QUERY:
+    {question}
+
+    Use the provided context to answer the provided user query. Only use the provided context to answer the query. If you do not know the answer, respond with "I don't know".
+    """
+    rag_prompt = ChatPromptTemplate.from_template(RAG_PROMPT)
+
+    # Create the retrieval augmented QA chain using the Qdrant retriever, RAG prompt, and OpenAI chat model
+    openai_chat_model = ChatOpenAI(model="gpt-3.5-turbo")
+    retrieval_augmented_qa_chain = (
+        {"context": itemgetter("question") | qdrant_retriever, "question": itemgetter("question")}
+        | RunnablePassthrough.assign(context=itemgetter("context"))
+        | {"response": rag_prompt | openai_chat_model, "context": itemgetter("context")}
+    )
+    print("Created retrieval augmented QA chain from uploaded PDF file =", filename_uploaded, "\n")
 
     return retrieval_augmented_qa_chain
